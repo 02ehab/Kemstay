d13bd49589fbeb52194b5f4c6b7af503119a35f1
Ehab <40764172+02ehab@users.noreply.github.com>
2025-06-21 09:21:32 +0300

    Update notifications.js

--- notifications.js
+++ notifications.js
@@ -33,3 +33,40 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 });
 
+// تحقق من حالة تسجيل الدخول من localStorage
+document.addEventListener("DOMContentLoaded", () => {
+  const authLink = document.getElementById("authLink");
+  const isLoggedIn = localStorage.getItem("isLoggedIn");
+
+  if (isLoggedIn === "true") {
+    authLink.textContent = "البروفايل";
+    authLink.href = "profile.html";
+  } else {
+    authLink.textContent = "تسجيل الدخول";
+    authLink.href = "login.html";
+  }
+});
+// بعد نجاح تسجيل الدخول
+//localStorage.setItem("isLoggedIn", "true");
+//window.location.href = "index.html";
+
+function logout() {
+  localStorage.removeItem("isLoggedIn");
+  window.location.href = "index.html";
+}
+
+//تغيير حالة تسجيل الدخول
+document.addEventListener("DOMContentLoaded", function () {
+  const authLink = document.getElementById("authLink");
+
+  const isLoggedIn = localStorage.getItem("isLoggedIn");
+
+  if (isLoggedIn === "true") {
+    authLink.textContent = "الملف الشخصي";
+    authLink.href = "profile.html";
+  } else {
+    authLink.textContent = "تسجيل الدخول";
+    authLink.href = "login.html";
+  }
+});
+
