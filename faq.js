df857a5b9e995c592422709f633352b42697064b
Ehab <40764172+02ehab@users.noreply.github.com>
2025-07-21 12:54:59 +0300

    Add files via upload

---
+++ faq.js
@@ -0,0 +1,34 @@
+function openMenu() {
+  document.getElementById("sideMenu").classList.add("open");
+}
+
+function closeMenu() {
+  document.getElementById("sideMenu").classList.remove("open");
+}
+
+
+//تغيير حالة تسجيل الدخول
+document.addEventListener("DOMContentLoaded", () => {
+  const authLinks = document.querySelectorAll(".auth-link");
+  const isLoggedIn = localStorage.getItem("isLoggedIn");
+
+  if (authLinks.length === 0) return; // ما فيش عناصر، نخرج بأمان
+
+  authLinks.forEach(link => {
+    if (isLoggedIn === "true") {
+      link.textContent = "الملف الشخصي";
+      link.href = "profile.html";
+    } else {
+      link.textContent = "تسجيل الدخول";
+      link.href = "login.html";
+    }
+  });
+});
+
+//faq
+const items = document.querySelectorAll(".faq-item");
+    items.forEach(item => {
+      item.querySelector(".question").addEventListener("click", () => {
+        item.classList.toggle("active");
+      });
+    });
---
+++ privacy.js
@@ -0,0 +1,27 @@
+function openMenu() {
+  document.getElementById("sideMenu").classList.add("open");
+}
+
+function closeMenu() {
+  document.getElementById("sideMenu").classList.remove("open");
+}
+
+
+//تغيير حالة تسجيل الدخول
+document.addEventListener("DOMContentLoaded", () => {
+  const authLinks = document.querySelectorAll(".auth-link");
+  const isLoggedIn = localStorage.getItem("isLoggedIn");
+
+  if (authLinks.length === 0) return; // ما فيش عناصر، نخرج بأمان
+
+  authLinks.forEach(link => {
+    if (isLoggedIn === "true") {
+      link.textContent = "الملف الشخصي";
+      link.href = "profile.html";
+    } else {
+      link.textContent = "تسجيل الدخول";
+      link.href = "login.html";
+    }
+  });
+});
+
