fc45dbd8d304da75de4512b4b82d32473378f590
Ehab <40764172+02ehab@users.noreply.github.com>
2025-07-26 08:53:36 +0300

    Add files via upload

---
+++ contract.js
@@ -0,0 +1,81 @@
+function openMenu() {
+  document.getElementById("sideMenu").classList.add("open");
+}
+
+function closeMenu() {
+  document.getElementById("sideMenu").classList.remove("open");
+}
+
+// تسجيل الدخول وإنشاء حساب
+function showLogin() {
+  document.getElementById("loginForm").classList.add("active");
+  document.getElementById("registerForm").classList.remove("active");
+  document.getElementById("btn-indicator").style.right = "0%";
+}
+
+function showRegister() {
+  document.getElementById("loginForm").classList.remove("active");
+  document.getElementById("registerForm").classList.add("active");
+  document.getElementById("btn-indicator").style.right = "50%";
+}
+function loginWithGoogle() {
+  alert("تم الضغط على تسجيل باستخدام Google (ربط Google OAuth يأتي لاحقًا)");
+}
+
+
+// صفحة تفاصيل الوحدة وحجز وهمي
+document.addEventListener("DOMContentLoaded", () => {
+  const reserveBtn = document.querySelector(".reserve-btn");
+  if (reserveBtn) {
+    reserveBtn.addEventListener("click", () => {
+      alert("تم إرسال طلب الحجز بنجاح! سيتم التواصل معك قريبًا.");
+    });
+  }
+});
+
+// تحقق من حالة تسجيل الدخول من localStorage
+document.addEventListener("DOMContentLoaded", () => {
+  const authLink = document.getElementById("authLink");
+  const isLoggedIn = localStorage.getItem("isLoggedIn");
+
+  if (isLoggedIn === "true") {
+    authLink.textContent = "البروفايل";
+    authLink.href = "profile.html";
+  } else {
+    authLink.textContent = "تسجيل الدخول";
+    authLink.href = "login.html";
+  }
+});
+// بعد نجاح تسجيل الدخول
+//localStorage.setItem("isLoggedIn", "true");
+//window.location.href = "index.html";
+
+function logout() {
+  localStorage.removeItem("isLoggedIn");
+  window.location.href = "index.html";
+}
+
+//تغيير حالة تسجيل الدخول
+document.addEventListener("DOMContentLoaded", () => {
+  const authLinks = document.querySelectorAll(".auth-link");
+  const isLoggedIn = localStorage.getItem("isLoggedIn");
+
+  if (authLinks.length === 0) return; // ما فيش عناصر، نخرج بأمان
+
+  authLinks.forEach(link => {
+    if (isLoggedIn === "true") {
+      link.textContent = "الملف الشخصي";
+      link.href = "profile.html";
+    } else {
+      link.textContent = "تسجيل الدخول";
+      link.href = "login.html";
+    }
+  });
+});
+
+// منع دخول الصفحات بدون تسجيل
+const isLoggedIn = localStorage.getItem("isLoggedIn");
+  if (!isLoggedIn) {
+    // تحويل المستخدم لتسجيل الدخول
+    window.location.href = "login.html";
+  }
